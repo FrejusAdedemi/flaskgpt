0403f776b7b0c02cde1b01326cc8a7595d8652b3
Frejus Adedemi <frejusadedemi2@gmail.com>
2025-04-15 20:17:44 +0200

    🚀 Version clean avec Railway.toml

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+import openai
+from dotenv import load_dotenv
+from flask import Flask, Response, render_template, request
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/prompt', methods=['POST'])
+def prompt():
+    messages = request.json['messages']
+    conversation = build_conversation_dict(messages=messages)
+
+    return Response(event_stream(conversation), mimetype='text/event-stream')
+
+
+def event_stream(conversation: list[dict]) -> str:
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=conversation,
+        stream=True
+    )
+
+    for line in response:
+        text = getattr(line.choices[0].delta, 'content', '')
+        if text:
+            yield text
+#
+#
+def build_conversation_dict(messages: list) -> list[dict]:
+    return [
+        {"role": "user" if i % 2 == 0 else "assistant", "content": message}
+        for i, message in enumerate(messages)
+    ]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
